28ac70f3b71c8ad1274a41168e016a2deb8f6f98
ben200045 <frozen123>
2017-08-21 14:13:50 +0800

    Add project files.

---
+++ Linklist1/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Linklist1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            {
+                nodeClass.introSpiel();
+
+                myList list = new myList();
+                list.AddSorted(632);
+                list.AddSorted(258);
+                list.AddSorted(478);
+                list.AddSorted(256);
+                list.AddSorted(214);
+                list.AddSorted(982);
+                list.AddSorted(471);
+                list.AddSorted(458);
+                list.AddSorted(206);
+                list.AddSorted(567);
+
+                int userSelection;
+
+                try
+                {
+                    userSelection = Convert.ToInt32(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("Woah there friend, real inputs only please");
+                }
+                userSelection = Convert.ToInt32(Console.ReadLine());
+                switch (userSelection)
+                {
+                    case 1:
+                        {
+                            list.Print();
+                            break;
+                        }
+
+                    case 2:
+                        {
+                            int newAdd;
+                            Console.WriteLine("Enter the Addition to the LinkedList (The end)");
+                            newAdd = Convert.ToInt32(Console.ReadLine());
+                            list.AddToEnd(newAdd);
+                            break;
+                        }
+
+                    case 3:
+                        {
+                            
+                            break;
+                        }
+
+                    case 6:
+                        {
+                            Environment.Exit(0);
+                            break;
+                        }
+
+                    default:
+                        {
+                            Console.WriteLine("Woah there friend, that's not an available command, try again");
+                            break;
+                        }
+                }
+
+
+
+                Console.ReadLine();
+            }
+            
+            
+      
+
+        }
+    }
+
+}
---
+++ Linklist1/nodeClass.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linklist1
+{
+    public class nodeClass
+    {
+
+        public static void introSpiel()
+        {
+            Console.WriteLine("Hi and welcome to the Assessment 1 experience |");
+            Console.WriteLine("-----------------------------------------------");
+            Console.WriteLine("Here in you'll find the required resources to add, delete, find specific data in and view a linklist");
+            Console.WriteLine("The Scenario this program will be following will be Scenario 1, Adjacent Neighbours in a street");
+            Console.WriteLine("");
+            Console.WriteLine("To Continue please select one of the following options");
+            Console.WriteLine("1- the list is displayed in forward viewing fashion   --->");
+            Console.WriteLine("2- the list is displayed in reverse viewing fashion  <--- ");
+            Console.WriteLine("3- ");
+        }
+
+        public int data;
+        public nodeClass Next;
+        public nodeClass Previous;
+
+        public nodeClass(int i)
+        {
+            data = i;
+            Next = null;
+            Previous = null;
+        }
+
+        public void AddToEnd(int data)
+        {
+            if (Next == null)
+            {
+                Next = new nodeClass(data);
+            }
+            else
+            {
+                Next.AddToEnd(data);
+            }
+        }
+
+        
+
+        public void Print()
+        {
+            Console.Write("|" + data);
+            if(Next != null)
+            {
+                Console.Write("|-->");
+                Next.Print();
+            }
+            else
+            {
+                Console.WriteLine("|");
+            }
+        }
+
+        public void AddSorted(int data)
+        {
+            
+            if (Next == null)
+            {
+                Next = new nodeClass(data);
+            }
+            else if (data < Next.data)
+            {
+                nodeClass temp = new nodeClass(data);
+                temp.Next = this.Next;
+                this.Next = temp;
+            }
+            else
+            {
+                Next.AddSorted(data);
+            }
+        }
+
+        public static void Restart()
+        {
+            
+        }
+    }
+
+    public class myList
+    {
+        public nodeClass headNode;
+
+        public myList()
+        {
+            headNode = null;
+        }
+
+        public void AddToEnd(int data)
+        {
+            if (headNode == null)
+            {
+                headNode = new nodeClass(data);
+            }
+            else
+            {
+                headNode.AddToEnd(data);
+            }
+        }
+
+        public void AddSorted (int data)
+        {
+            if (headNode == null)
+            {
+                headNode = new nodeClass(data);
+            }
+            else if (data < headNode.data)
+            {
+                AddToBegginning(data);
+            }
+            else
+            {
+                headNode.AddSorted(data);
+            }
+        }
+
+        public void AddToBegginning(int data)
+        {
+            if (headNode == null)
+            {
+                headNode = new nodeClass(data);
+            }
+            else
+            {
+                nodeClass temp = new nodeClass(data);
+                temp.Next = headNode;
+                headNode = temp;
+            }
+        }
+
+        
+
+        public void Print()
+        {
+            if (headNode != null)
+            {
+                headNode.Print();
+            }
+        }
+    }
+}
